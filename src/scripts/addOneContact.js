70d2d1f71ecef9fcdee447d5cb037cc53761602f
olexandra0pryshchepa <130248875+olexandra0pryshchepa@users.noreply.github.com>
2024-09-21 20:45:44 +0200

    first commit

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();  
+    const newContact = createFakeContact();  
+    contacts.push(newContact); 
+    await writeContacts(contacts); 
+    console.log('Contact added:', newContact);
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();  
+    const newContacts = Array.from({ length: number }, () => createFakeContact());  
+    contacts.push(...newContacts); 
+    await writeContacts(contacts);  
+    console.log(`${number} contacts generated.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
